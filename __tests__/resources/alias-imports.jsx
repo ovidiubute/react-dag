f64ea76971e669bd18d7da2a8e3da6e1f01c850b
ovidiubute <ovidiu.bute@gmail.com>
2017-08-14 09:24:53 +0300

    Ignore alias imports, add JSDoc, define Import interface

---
+++ __tests__/resources/alias-imports.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button as UiButton } from 'third-party-ui/button';
+
+export default class Foo extends React.Component {
+  render() {
+    return (
+      <div>
+        <UiButton />
+      </div>
+    );
+  }
+}
--- src/parser.js
+++ src/parser.js
@@ -1,5 +1,11 @@
 const babylon = require('babylon');
 
+/**
+ * @typedef {Object} Import
+ * @property {String} name The name of the imported object
+ * @property {String} path The path as specified in the import statement 
+ */
+
 const parse = sourceCode => {
   return babylon.parse(sourceCode, {
     sourceType: 'module',
@@ -7,13 +13,24 @@ const parse = sourceCode => {
   });
 };
 
+/**
+ * Parse an AST and return a list of all the imports.
+ * @param {Object} ast AST as obtained from Babylon
+ * @return {Import[]} List of imports
+ */
 const getImports = ast => {
   return ast.program.body
     .filter(node => node.type === 'ImportDeclaration')
     .reduce((accum, node) => {
       for (let specifier of node.specifiers) {
+        // Check for aliased imports
+        let importName = specifier.local.name;
+        if (specifier.imported) {
+          importName = specifier.imported.name;
+        }
+
         accum.push({
-          localIdentifier: specifier.local.name,
+          importName,
           importPath: node.source.value,
         });
       }
