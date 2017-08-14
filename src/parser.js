ecbff6b086dc9e3bfa4500aae3387c0a49fa7c75
ovidiubute <ovidiu.bute@gmail.com>
2017-08-14 11:19:43 +0300

    Improve asset regex

--- src/parser.js
+++ src/parser.js
@@ -44,7 +44,7 @@ const getImports = ast => {
 
 const jsModuleImport = theImport => {
   return !theImport.path.match(
-    /\.(css|less|sass|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$/i
+    /\.(json|xml|css|less|sass|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$/i
   );
 };
 
